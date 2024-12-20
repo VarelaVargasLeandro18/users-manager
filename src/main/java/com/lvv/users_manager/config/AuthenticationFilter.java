e795b8d9c3493835c6aa1b5b98753dd4ad999c23
lvarela <varelavargasleandro@outlook.com.ar>
2024-12-20 15:26:57 -0300

    feature/LogInAndSecurity - Added filter for authentification and changed security config.

---
+++ src/main/java/com/lvv/users_manager/config/AuthenticationFilter.java
@@ -0,0 +1,100 @@
+package com.lvv.users_manager.config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.lvv.users_manager.exceptions.JwtNoPresentException;
+import com.lvv.users_manager.exceptions.UserApplicationNotMatchesException;
+import com.lvv.users_manager.utils.JWTUtils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String ROLE_PREFIX = "ROLE_";
+    private static final String LOGIN_URI = "/auth/log-in";
+
+    private final HandlerExceptionResolver exceptionResolver;
+
+    @Value("${JWT-SECRET}")
+    private String jwtSecret;
+
+    @Value("${APP-UID}")
+    private String appUID;
+
+    public AuthenticationFilter(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
+        this.exceptionResolver = exceptionResolver;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {        
+        String jwt = getJwtFromRequest(request);
+
+        try {
+            Claims jwtClaims = JWTUtils.getClaims(jwt, this.jwtSecret);
+            String username = jwtClaims.getSubject();
+            String jwtAppUID = (String) jwtClaims.get(JWTUtils.APPLICATION_CLAIM);
+
+            if (jwtAppUID == null || !jwtAppUID.equals(this.appUID)) {
+                throw new UserApplicationNotMatchesException();
+            }
+
+            List<String> rolesFromJWT = (List<String>) jwtClaims.get(JWTUtils.ROLES_CLAIM);
+            List<String> permissionsFromJWT = (List<String>) jwtClaims.get(JWTUtils.PERMISSIONS_CLAIM);
+
+            List<SimpleGrantedAuthority> authorities = rolesFromJWT.stream()
+                .map(role -> ROLE_PREFIX + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+            authorities.addAll(permissionsFromJWT.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);   
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);   
+            filterChain.doFilter(request, response);      
+        } catch( Exception ex ) {
+            exceptionResolver.resolveException(request, response, null, ex);
+        }
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        throw new JwtNoPresentException();
+    }
+
+    @Override
+    protected boolean shouldNotFilter(@NonNull HttpServletRequest request) {
+        return request.getServletPath().equals(LOGIN_URI);
+    }
+
+
+}
---
+++ src/main/java/com/lvv/users_manager/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.lvv.users_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String LOGIN_URL ="/auth/log-in";
+
+    private final AuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(LOGIN_URL).permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+}
