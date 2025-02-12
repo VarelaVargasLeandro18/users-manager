cd273ccddf72bf467344fe65f656b28d63ea0799
lvarela <varelavargasleandro@outlook.com.ar>
2025-02-11 21:41:27 -0300

    feature/UsersAllData - Deleted ApplicationRepository from UsersServices

--- src/main/java/com/lvv/users_manager/services/impl/UsersServicesImpl.java
+++ src/main/java/com/lvv/users_manager/services/impl/UsersServicesImpl.java
@@ -2,13 +2,11 @@ package com.lvv.users_manager.services.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.lvv.users_manager.entities.Application;
 import com.lvv.users_manager.entities.User;
 import com.lvv.users_manager.exceptions.EntityAlreadyExistsException;
-import com.lvv.users_manager.mappers.UserMapper;
 import com.lvv.users_manager.helpers.PasswordHasherHelper;
+import com.lvv.users_manager.mappers.UserMapper;
 import com.lvv.users_manager.models.UserDTO;
-import com.lvv.users_manager.repositories.ApplicationRepository;
 import com.lvv.users_manager.repositories.UsersRepository;
 import com.lvv.users_manager.services.UsersServices;
 
@@ -19,20 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 public class UsersServicesImpl extends CrudServiceAbstract<UsersRepository, User, Integer, UserDTO> implements UsersServices {
 
     private final UserMapper userMapper;
-    private final ApplicationRepository applicationRepository;
     private final PasswordHasherHelper passwordHasherHelper;
 
-    public UsersServicesImpl(UsersRepository usersRepository, UserMapper userMapper, ApplicationRepository applicationRepository, PasswordHasherHelper passwordHasherHelper) {
+    public UsersServicesImpl(UsersRepository usersRepository, UserMapper userMapper, PasswordHasherHelper passwordHasherHelper) {
         super(usersRepository);
         this.userMapper = userMapper;
-        this.applicationRepository = applicationRepository;
         this.passwordHasherHelper = passwordHasherHelper;
     }
 
     @Override
     protected UserDTO toDTO(User entity) {
-        Application application = this.applicationRepository.findById(entity.getApplication().getUuid()).orElse(null);
-        entity.setApplication(application);
         return this.userMapper.toDTO(entity);
     }
 
