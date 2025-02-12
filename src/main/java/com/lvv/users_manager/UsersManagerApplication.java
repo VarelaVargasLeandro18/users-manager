0a5de7aae6d03fdb6a61f883073ddcb7f2140ec7
lvarela <varelavargasleandro@outlook.com.ar>
2025-02-11 21:38:04 -0300

    Configuration - Set default timezone to UTC-3

--- src/main/java/com/lvv/users_manager/UsersManagerApplication.java
+++ src/main/java/com/lvv/users_manager/UsersManagerApplication.java
@@ -1,7 +1,11 @@
 package com.lvv.users_manager;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class UsersManagerApplication {
@@ -9,5 +13,10 @@ public class UsersManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersManagerApplication.class, args);
 	}
+
+	@PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
+    }
 
 }
